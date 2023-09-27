0997d214b4a039b308b0394e1413f54b30ec9f11
Heewon Lee <pingpingy@kaist.ac.kr>
2023-09-27 23:54:50 +0900

    [scrap] 중구, 광진구, 동대문구 스크랩. type hint 제거

--- scrap.py
+++ scrap.py
@@ -1,28 +1,30 @@
-import requests
-from bs4 import BeautifulSoup
-from bs4.element import Tag, NavigableString
 import os
 from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 import gspread
-from urllib.parse import urlparse, ParseResult
+
+import requests
+from bs4 import BeautifulSoup
+
+from urllib.parse import urlparse
 
 
 # 구글로부터 권한을 요청할 어플리케이션 목록
 # 변경 시 token.json 삭제 후 재인증 필요
-SCOPES: list[str] = ["https://www.googleapis.com/auth/spreadsheets"]
-HEADERS: dict = {
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+# HTTPS 요청에 포함해줄 헤더
+HEADERS = {
 	"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
 }
 
-def google_authorization() -> gspread.client.Client:
+def google_authorization():
     '''Google Sheets API 활용을 위한 인증 정보 요청
     credentials.json 파일을 토대로 인증을 요청하되, token.json 파일이 존재할 경우 거기에 저장된 정보 활용
     
     :return: gspread.client.Client 인스턴스'''
 
-    creds: Credentials = None
+    creds = None
 
     # 이미 저장된 인증 정보가 있는지 확인
     if os.path.exists('token.json'):
@@ -33,7 +35,7 @@ def google_authorization() -> gspread.client.Client:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow: InstalledAppFlow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            flow= InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
@@ -41,37 +43,108 @@ def google_authorization() -> gspread.client.Client:
     return gspread.authorize(creds)
 
 
-def scrap_from_site(url: str) -> list[dict]:
-    '''주어진 의회 url로부터 의원 상세약력을 스크랩하여 반환
-    
-    :param url: 의회 사이트의 의원 목록 사이트 url
+def scrap_junggu(url):
+    '''서울시 중구의회 페이지에서 의원 상세약력 스크랩
+
+    :param url: 중구의회 의원 목록 사이트 url
     :return: 의원들의 이름과 정당 데이터를 담은 dict의 list
     '''
-
-    print(url)
-    parliment_response: requests.Response = requests.get(url, verify=False, headers=HEADERS)
-    parliment_soup: BeautifulSoup = BeautifulSoup(parliment_response.text, 'html.parser')
-    ret: list[dict] = []
+    
+    parliment_response = requests.get(url, verify=False, headers=HEADERS)
+    parliment_soup = BeautifulSoup(parliment_response.text, 'html.parser')
+    ret = []
 
     # 프로필 링크 스크랩을 위해 base_url 추출
-    parsed_url: ParseResult = urlparse(url)
-    base_url: str = f"{parsed_url.scheme}://{parsed_url.netloc}"
+    parsed_url = urlparse(url)
+    base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
 
     for profile in parliment_soup.find_all('div', class_='profile'):
-        name: str = profile.find('em', class_='name').text
-        party: str = "정당 정보 없음"
+        name = profile.find('em', class_='name').text
+        party = '정당 정보 없음'
 
         # 프로필보기 링크 가져오기
-        profile_link: dict = profile.find('a', class_='start')
+        profile_link = profile.find('a', class_='start')
         if profile_link:
-            profile_url: str = base_url + profile_link['href']
-            profile_response: requests.Response = requests.get(profile_url, verify=False, headers=HEADERS)
-            profile_soup: BeautifulSoup = BeautifulSoup(profile_response.text, 'html.parser')
-            
-            # "소속정당" 정보 추출
-            party_info = profile_soup.find('em', text='소속정당 : ').find_next('span')
+            profile_url = base_url + profile_link['href']
+            profile_response = requests.get(profile_url, verify=False, headers=HEADERS)
+            profile_soup = BeautifulSoup(profile_response.text, 'html.parser')
+
+            party_info = profile_soup.find('em', text='소속정당 : ')
             if party_info:
-                party = party_info.string
+                party = party_info.find_next('span').string
+
+        ret.append({
+            "이름": name,
+            "정당": party
+        })
+
+    return ret
+
+
+def scrap_gwangjingu(url):
+    '''서울시 광진구 페이지에서 의원 상세약력 스크랩
+
+    :param url: 광진구의회 의원 목록 사이트 url
+    :return: 의원들의 이름과 정당 데이터를 담은 dict의 list
+    '''
+    
+    parliment_response = requests.get(url, verify=False, headers=HEADERS)
+    parliment_soup = BeautifulSoup(parliment_response.text, 'html.parser')
+    ret = []
+
+    # # 프로필 링크 스크랩을 위해 base_url 추출
+    # parsed_url = urlparse(url)
+    # base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
+
+    for profile in parliment_soup.find_all('div', class_='profile'):
+        name = profile.find('div', class_='name').find_next('strong').string
+        party = '정당 정보 없음'
+
+        party_info = profile.find('em', string = '소속정당')
+        if party_info:
+            party = party_info.find_next('span').find_next('span').string
+
+        ret.append({
+            "이름": name,
+            "정당": party
+        })
+
+    return ret
+
+
+def scrap_dongdaemungu(url):
+    '''서울시 동대문구 페이지에서 의원 상세약력 스크랩
+
+    :param url: 동대문구의회 의원 목록 사이트 url
+    :return: 의원들의 이름과 정당 데이터를 담은 dict의 list
+    '''
+    
+    parliment_response = requests.get(url, verify=False, headers=HEADERS)
+    parliment_response.encoding = 'euc-kr'
+    parliment_soup = BeautifulSoup(parliment_response.text, 'html.parser')
+    ret = []
+
+    # 프로필 링크 스크랩을 위해 base_url 추출
+    parsed_url = urlparse(url)
+    base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
+
+    for profile in parliment_soup.find_all('div', class_='intro_text tm_lg_6'):
+        name = profile.find('p', class_='intro_text_title').string.strip().split(' ')[0]
+        party = '정당 정보 없음'
+
+        # 프로필보기 링크 가져오기
+        profile_link = profile.find('a')
+        if profile_link:
+            profile_url = base_url + '/assemblyman/greeting/menu02.asp?assembly_id=' + profile_link['href'][1:]
+            profile_response = requests.get(profile_url, verify=False, headers=HEADERS)
+            profile_response.encoding = 'euc-kr'
+            profile_soup = BeautifulSoup(profile_response.text, 'html.parser')
+
+            profile_info = profile_soup.find('div', class_='profileTxt')
+            if profile_info:
+                profile_string = profile_info.get_text().strip().split('\xa0')
+                idx = profile_string.index('소속정당')
+                party = profile_string[idx + 2]
 
         ret.append({
             "이름": name,
@@ -92,10 +165,9 @@ def main() -> None:
     # 데이터 가져오기
     data: list[dict] = worksheet.get_all_records()
 
-    for tuple in data:
-        num_parsed: int = len(scrap_from_site(tuple['상세약력 링크']))
-        print(f"{tuple['광역구분']} {tuple['기초자치단체명']}: {num_parsed}")
-    # print(scrap_from_site('http://council.ddm.go.kr/citizen/menu1.asp'))
+    print(scrap_junggu(data[1]['상세약력 링크']))
+    print(scrap_gwangjingu(data[4]['상세약력 링크']))
+    print(scrap_dongdaemungu(data[5]['상세약력 링크']))
 
 
 if __name__ == '__main__':
