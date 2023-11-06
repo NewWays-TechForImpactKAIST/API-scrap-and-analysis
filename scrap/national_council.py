a900bce4897ab2c85c9748e1fd3880b3d7486759
Heewon Lee <pingpingy@kaist.ac.kr>
2023-11-06 18:59:38 +0900

    [Scrap] 국회의원 스크랩 함수 수정

--- scrap/national_council.py
+++ scrap/national_council.py
@@ -7,13 +7,12 @@ import xml.etree.ElementTree as ET
 BASE_DIR = os.path.join(os.path.dirname(__file__), os.pardir)
 
 
-def scrap_national_council(cd: int) -> ScrapResult:
-    """열린국회정보 Open API를 이용해 역대 국회의원 인적사항 스크랩
+def scrap_national_council() -> ScrapResult:
+    """열린국회정보 Open API를 이용해 제21대 국회의원 인적사항 스크랩
     _data 폴더에 assembly_api_key.json 파일을 만들어야 하며,
     해당 JSON은 {"key":"(Open API에서 발급받은 인증키)"} 꼴을 가져야 한다.
     https://open.assembly.go.kr/portal/data/service/selectAPIServicePage.do/OBL7NF0011935G18076#none
 
-    :param cd: 국회의원 대수. 제21대 국회의원을 스크랩하고자 하면 21
     :return: 국회의원들의 이름과 정당 데이터를 담은 ScrapResult 객체
     """
 
@@ -25,7 +24,7 @@ def scrap_national_council(cd: int) -> ScrapResult:
     with open(key_json_path, "r") as key_json:
         assembly_key = json.load(key_json)["key"]
 
-    request_url = f"https://open.assembly.go.kr/portal/openapi/nwvrqwxyaytdsfvhu?KEY={assembly_key}&pSize=500&UNIT_CD={cd + 100000}"
+    request_url = f"https://open.assembly.go.kr/portal/openapi/nwvrqwxyaytdsfvhu?KEY={assembly_key}&pSize=500"
     response = requests.get(request_url)
 
     if response.status_code != 200:
@@ -47,4 +46,5 @@ def scrap_national_council(cd: int) -> ScrapResult:
 
 
 if __name__ == "__main__":
-    print(scrap_national_council(21))
+    result = scrap_national_council()
+    print(len(result.councilors))
