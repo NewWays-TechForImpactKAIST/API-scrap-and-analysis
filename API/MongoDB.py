18a7453bbcf520c7a579c38c9211cc1c146ad494
happycastle114 <happycastle114@users.noreply.github.com>
2023-11-20 11:51:05 +0000

    Formatted with black

--- API/MongoDB.py
+++ API/MongoDB.py
@@ -30,7 +30,7 @@ class Councilor:
             eduId=data.get("eduId"),
             edu=data.get("edu"),
         )
-    
+
     def to_dict(self):
         return {
             "sdName": self.sdName,
