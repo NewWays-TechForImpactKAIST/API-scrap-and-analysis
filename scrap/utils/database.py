1cff3ee0cc7da75f1e61116a254aa7bf34ce0613
Re-st <garypark@kaist.ac.kr>
2023-11-23 16:47:44 +0900

    [scrap] 신규의원 후보중에 찾기. 아직 후보목록이 mongodb에 없어서 테스트는 하지 않음

--- scrap/utils/database.py
+++ scrap/utils/database.py
@@ -33,6 +33,7 @@ def save_to_database(record: ScrapResult) -> bool:
     # TODO: DB에 없던 새로운 의원 핸들링
     update_councilors(collection, cid, other_councilors)
     remove_councilors(collection, resigned_councilors)
+    add_councilors(collection, cid, new_councilors, str(record.council_type))
 
     return True
 
@@ -95,4 +96,33 @@ def remove_councilors(collection, councilors: List[Councilor]) -> None:
     for councilor in councilors:
         collection.delete_one(
             {"localId": councilor["localId"], "name": councilor["name"]}
+        )
+
+def add_councilors(collection, cid: int, councilors: List[Councilor], council_type: str) -> None:
+    """
+    신규 의원을 검사합니다. 기존 비례대표의 사퇴로 새 비례대표가 바로 추가되었을
+    가능성을 보고, 아닐 시에는 사용자에게 보고합니다.
+    :param collection: MongoDB 컬렉션
+    :param councilors: 업데이트할 의원 리스트
+    :param council_type: local_councilor 등등, 의원의 종류 
+    """
+    db = client[str(MongoDBSecrets.database_name)]
+    candcoll = db[council_type + "_cand"] # 예: local_councilor_cand
+    for councilor in councilors:
+        # find the councilor in candidate
+        query = {"localId": cid, "name": councilor.name}
+        all_documents = list(candcoll.find(query))
+        assert(len(all_documents) <= 1, "신규의원을 같은 의회의 후보중에 찾는데, 동명이인이 있네요.")
+        if len(all_documents) == 0:
+            # no candidate found, report to user
+            print(f"신규의원 {councilor.name}을 찾을 수 없습니다.")
+            continue
+        found_document = all_documents[0]
+        age = found_document.get("age")
+        # birthday = found_document.get("birthday")
+        councilor.age = age
+        collection.update_one(
+            {"localId": cid, "name": councilor.name},
+            {"$set": asdict(councilor)},
+            upsert=False,
         )
