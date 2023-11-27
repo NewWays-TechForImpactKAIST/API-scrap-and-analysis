46564ede6bb3e058dad5817bb0459a60f2a61755
Re-st <Re-st@users.noreply.github.com>
2023-11-27 10:46:31 +0000

    Formatted with black

--- analysis/age/main.py
+++ analysis/age/main.py
@@ -26,6 +26,7 @@ councilordict = {
 
 personDB = client["council"]
 
+
 def run_by_excel(cluster_by, filenames, N=5, folder_name="To_be_filled"):
     assert cluster_by in ["sdName", "wiwName"]
     level = 1 if cluster_by == "sdName" else 2
@@ -56,6 +57,8 @@ def run_by_excel(cluster_by, filenames, N=5, folder_name="To_be_filled"):
             method=method,
         )
         cluster(df, N, basedic)
+
+
 # def main(N=5):
 #     run_by_excel("sdName", ["[당선][시도의원].xlsx", "[당선][광역의원비례대표].xlsx"])
 #     run_by_excel("sdName", ["[후보][시도의원].xlsx", "[후보][광역의원비례대표].xlsx"])
@@ -63,6 +66,7 @@ def run_by_excel(cluster_by, filenames, N=5, folder_name="To_be_filled"):
 #     run_by_excel("sdName", ["[후보][구시군의회의원].xlsx", "[후보][기초의원비례대표].xlsx"])
 #     run_by_excel("wiwName", ["[당선][구시군의회의원].xlsx", "[당선][기초의원비례대표].xlsx"])
 #     run_by_excel("wiwName", ["[후보][구시군의회의원].xlsx", "[후보][기초의원비례대표].xlsx"])
+
 
 def run_by_mongo(cluster_by, is_elected, councilorType, N=5):
     assert cluster_by in ["sdName", "wiwName"]
@@ -73,12 +77,27 @@ def run_by_mongo(cluster_by, is_elected, councilorType, N=5):
     cursor = personDB[councilorType].find()
     if level == 1:
         for person in cursor:
-            data.append({"year": person.get("year"), "sdName": person.get("sdName"), "name": person.get("name"),
-                        "age": person.get("age"), "gender": person.get("gender")})
+            data.append(
+                {
+                    "year": person.get("year"),
+                    "sdName": person.get("sdName"),
+                    "name": person.get("name"),
+                    "age": person.get("age"),
+                    "gender": person.get("gender"),
+                }
+            )
     else:
         for person in cursor:
-            data.append({"year": person.get("year"), "sdName": person.get("sdName"), "wiwName": person.get("wiwName"),
-                        "name": person.get("name"), "age": person.get("age"), "gender": person.get("gender")})
+            data.append(
+                {
+                    "year": person.get("year"),
+                    "sdName": person.get("sdName"),
+                    "wiwName": person.get("wiwName"),
+                    "name": person.get("name"),
+                    "age": person.get("age"),
+                    "gender": person.get("gender"),
+                }
+            )
 
     df = pd.DataFrame(data)
     df = df.sort_values(by="age")
@@ -90,15 +109,17 @@ def run_by_mongo(cluster_by, is_elected, councilorType, N=5):
             level=level,
             method=method,
         )
-        cluster(df, N, basedic, clean_flag = True)
+        cluster(df, N, basedic, clean_flag=True)
+
 
 def main(N=5):
     # 세종시의 경우 어느 순간 승급하기 때문에 sdName을 먼저 해야, sdName이 cluster 시작 때 밀려도 괜챃다. (cluster 함수 참조)
-    run_by_mongo("sdName", is_elected = True, councilorType = "metro_councilor")
-    run_by_mongo("sdName", is_elected = False, councilorType = "metro_councilor")
-    run_by_mongo("sdName", is_elected = True, councilorType = "local_councilor")
-    run_by_mongo("sdName", is_elected = False, councilorType = "local_councilor")
-    run_by_mongo("wiwName", is_elected = True, councilorType = "local_councilor")
-    run_by_mongo("wiwName", is_elected = False, councilorType = "local_councilor")
+    run_by_mongo("sdName", is_elected=True, councilorType="metro_councilor")
+    run_by_mongo("sdName", is_elected=False, councilorType="metro_councilor")
+    run_by_mongo("sdName", is_elected=True, councilorType="local_councilor")
+    run_by_mongo("sdName", is_elected=False, councilorType="local_councilor")
+    run_by_mongo("wiwName", is_elected=True, councilorType="local_councilor")
+    run_by_mongo("wiwName", is_elected=False, councilorType="local_councilor")
+
 
 main()
