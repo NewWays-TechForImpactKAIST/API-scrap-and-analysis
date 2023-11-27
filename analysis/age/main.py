9b407ed41d4e3f7dbe9b40f498ed53787ef77409
Re-st <garypark@kaist.ac.kr>
2023-11-27 19:45:18 +0900

    [analysis] MongoDB에서 데이터 불러오도록 작성. 추후 mongoDB councilor에 year 추가되면 테스트 필요

--- analysis/age/main.py
+++ analysis/age/main.py
@@ -6,6 +6,7 @@ from matplotlib import font_manager
 from analysis.age.most_common_age_group import most_common_age_group
 from analysis.age.hist_groups import cluster
 from analysis.age import BasicArgument
+from db.client import client
 
 # 경고 무시
 warnings.filterwarnings("ignore", category=FutureWarning)
@@ -23,9 +24,9 @@ councilordict = {
     "기초의원비례대표": "local_councilor",
 }
 
+personDB = client["council"]
 
-def run(cluster_by, filenames, N=5, folder_name="To_be_filled"):
-    ## TO-DO: excel말고 mongodb에서 받아오도록 합니다.
+def run_by_excel(cluster_by, filenames, N=5, folder_name="To_be_filled"):
     assert cluster_by in ["sdName", "wiwName"]
     level = 1 if cluster_by == "sdName" else 2
     datadir = os.path.join(BASE_DIR, "_data", folder_name)
@@ -38,6 +39,7 @@ def run(cluster_by, filenames, N=5, folder_name="To_be_filled"):
     else:
         df = df[["sgId", "sdName", "wiwName", "name", "age", "gender"]]
     df = df.sort_values(by="age")
+    df["year"] = df["sgId"] // 10000
     is_elected = (
         True
         if "당선" in d
@@ -54,15 +56,49 @@ def run(cluster_by, filenames, N=5, folder_name="To_be_filled"):
             method=method,
         )
         cluster(df, N, basedic)
+# def main(N=5):
+#     run_by_excel("sdName", ["[당선][시도의원].xlsx", "[당선][광역의원비례대표].xlsx"])
+#     run_by_excel("sdName", ["[후보][시도의원].xlsx", "[후보][광역의원비례대표].xlsx"])
+#     run_by_excel("sdName", ["[당선][구시군의회의원].xlsx", "[당선][기초의원비례대표].xlsx"])
+#     run_by_excel("sdName", ["[후보][구시군의회의원].xlsx", "[후보][기초의원비례대표].xlsx"])
+#     run_by_excel("wiwName", ["[당선][구시군의회의원].xlsx", "[당선][기초의원비례대표].xlsx"])
+#     run_by_excel("wiwName", ["[후보][구시군의회의원].xlsx", "[후보][기초의원비례대표].xlsx"])
 
+def run_by_mongo(cluster_by, is_elected, councilorType, N=5):
+    assert cluster_by in ["sdName", "wiwName"]
+    level = 1 if cluster_by == "sdName" else 2
+    data = []
+    if not is_elected:
+        councilorType = councilorType + "_candidate"
+    cursor = personDB[councilorType].find()
+    if level == 1:
+        for person in cursor:
+            data.append({"year": person.get("year"), "sdName": person.get("sdName"), "name": person.get("name"),
+                        "age": person.get("age"), "gender": person.get("gender")})
+    else:
+        for person in cursor:
+            data.append({"year": person.get("year"), "sdName": person.get("sdName"), "wiwName": person.get("wiwName"),
+                        "name": person.get("name"), "age": person.get("age"), "gender": person.get("gender")})
+
+    df = pd.DataFrame(data)
+    df = df.sort_values(by="age")
+
+    for method in ["kmeans", "equal"]:
+        basedic = BasicArgument(
+            councilorType=councilorType,
+            is_elected=is_elected,
+            level=level,
+            method=method,
+        )
+        cluster(df, N, basedic, clean_flag = True)
 
 def main(N=5):
-    run("sdName", ["[당선][시도의원].xlsx", "[당선][광역의원비례대표].xlsx"])
-    run("sdName", ["[후보][시도의원].xlsx", "[후보][광역의원비례대표].xlsx"])
-    run("sdName", ["[당선][구시군의회의원].xlsx", "[당선][기초의원비례대표].xlsx"])
-    run("sdName", ["[후보][구시군의회의원].xlsx", "[후보][기초의원비례대표].xlsx"])
-    run("wiwName", ["[당선][구시군의회의원].xlsx", "[당선][기초의원비례대표].xlsx"])
-    run("wiwName", ["[후보][구시군의회의원].xlsx", "[후보][기초의원비례대표].xlsx"])
-
+    # 세종시의 경우 어느 순간 승급하기 때문에 sdName을 먼저 해야, sdName이 cluster 시작 때 밀려도 괜챃다. (cluster 함수 참조)
+    run_by_mongo("sdName", is_elected = True, councilorType = "metro_councilor")
+    run_by_mongo("sdName", is_elected = False, councilorType = "metro_councilor")
+    run_by_mongo("sdName", is_elected = True, councilorType = "local_councilor")
+    run_by_mongo("sdName", is_elected = False, councilorType = "local_councilor")
+    run_by_mongo("wiwName", is_elected = True, councilorType = "local_councilor")
+    run_by_mongo("wiwName", is_elected = False, councilorType = "local_councilor")
 
 main()
