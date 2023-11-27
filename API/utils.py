84598c58f1107f1998857d5319f590655ffe2ec8
happycastle <41810556+happycastle114@users.noreply.github.com>
2023-11-27 15:20:48 +0900

    feat(add) metro councilor

--- API/utils.py
+++ API/utils.py
@@ -43,6 +43,7 @@ def save_to_mongo(data: List[dict], sgTypecode: str, where: str) -> None:
     # TODO: Support other types of councils
     if sgTypecode in ["6", "9"]:
         for entry in data:
+            entry["wiwName"] = change_local_name(entry["sdName"], entry["wiwName"])
             district_id = get_district_id(entry["sdName"], entry["wiwName"])
 
             if district_id:
@@ -51,6 +52,32 @@ def save_to_mongo(data: List[dict], sgTypecode: str, where: str) -> None:
                         "name": entry["name"],
                         "localId": district_id["localId"],
                         "metroId": district_id["metroId"],
+                    },
+                    {"$set": Councilor.from_dict(entry).to_dict()},
+                    upsert=True,
+                )
+            else:
+                print(
+                    f"Warning: '{entry['sdName']} {entry['wiwName']}'에 해당하는 지역 ID가 존재하지 않습니다."
+                )
+    elif sgTypecode == "5":
+        main_collection = db["metro_councilor"]
+        for entry in data:
+            entry["wiwName"] = change_local_name(entry["sdName"], entry["wiwName"])
+            district_id = get_district_id(entry["sdName"], entry["wiwName"])
+
+            if not district_id:
+                print(
+                    f"Warning: '{entry['sdName']} {entry['wiwName']}'에 해당하는 지역 ID가 존재하지 않습니다."
+                )
+                continue
+
+            if district_id:
+                main_collection.update_one(
+                    {
+                        "name": entry["name"],
+                        "local_id": district_id["local_id"],
+                        "metro_id": district_id["metro_id"],
                     },
                     {"$set": Councilor.from_dict(entry).to_dict()},
                     upsert=True,
@@ -95,3 +122,54 @@ def getLocalMetroMap() -> Dict[str, str]:
         }
         for item in result
     }
+
+def getLocalMetroMap() -> Dict[str, str]:
+    db = client["district"]
+    result = db["local_district"].aggregate(
+        [
+            {
+                "$project": {
+                    "localId": 1,
+                    "metroId": 1,
+                    "sdName": 1,
+                    "wiwName": 1,
+                }
+            },
+        ]
+    )
+    return {
+        (item["sdName"], item["wiwName"]): {
+            "local_id": item["localId"],
+            "metro_id": item["metroId"],
+        }
+        for item in result
+    }
+
+def change_local_name(sdName, wiwName):
+    """
+    1. 만약 '시' 와 '구'가 모두 wiwName에 있다면, '시' 까지만 쓰기
+    ex) '용인시수지구' (선거 단위) -> '용인시' (의회 단위)
+    2. 지역이 승급되면 이름 바꾸기
+    ex) '당진군' (~2011) -> '당진시' (2012~)
+    Keyword arguments:
+    argument -- string
+    Return: processed string
+    """
+    if (sdName, wiwName) in change_city_name:
+        return change_city_name[(sdName, wiwName)]
+    if "구" in wiwName and "시" in wiwName:
+        return wiwName.split("시")[0] + "시"
+    else:
+        return wiwName
+    
+change_city_name = {
+    ("충청남도", "당진군"): "당진시",
+    ("경상남도", "마산시"): "창원시",
+    ("경상남도", "진해시"): "창원시",
+    ("경기도", "여주군"): "여주시",
+    ("충청북도", "청원군"): "청주시",
+    ("인천광역시", "남구"): "미추홀구",
+}
+
+#
+change_lvl2to1 = {"연기군": "세종특별자치시"}
